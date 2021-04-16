39760e926dd543d5677f154003eef8f567efb608
dnvflf <dnvflf@163.com>
2021-04-16 15:16:16 +0800

    Create push_message.json.js
    推广，帮助大家移花接木！

---
+++ push_message.json.js
@@ -0,0 +1,6 @@
+{
+"content1":"<em style='color: gold;'>公告区：上游规则清空，请关闭护网大师！如有使用！https://youtu.be/HLzjLZqLrCI 关注油管视频与视频主共同探讨解决方法",
+"content2":"<em style='color: gold;'>公告区：近期DNS解析问题：<a href='https://www.bilibili.com/video/BV1qz4y1r7ke?share_source=copy_web' target='_blank'>DNS设置</a></em>",
+"content3":"<em style='color: gold;'>交流频道：<a href='https://t.me/dnvflfvlog' target='_blank'>Merlinclash</a></em>",
+"content4":"<marquee><em style='color: gold;'>亻弗系老木几土昜：</em>&nbsp;&nbsp;<a href='https://ikuuu.ltd/auth/register?code=f6Fz' target='_blank'>【ikuuu--便宜厚道、丰俭由人】</a></marquee>"
+}
